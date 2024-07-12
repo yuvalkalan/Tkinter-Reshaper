f7ada82eb58fceed3c3483425c1d25088f7e849d
Yuval Kalanthroff <51821966+yuvalkalan@users.noreply.github.com>
2024-07-12 15:38:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
 import string
-import tkinter as tk
+
+
+REVERSE = {'(': ')', ')': '('}
+
+
+def is_both(char):
+    return char == ' ' or '0' <= char <= '9' or char in string.punctuation
 
 
 def is_hebrew(char):
-    return 'א' <= char <= 'ת' or char == ' ' or '0' <= char <= '9' or char in string.punctuation
+    return 'א' <= char <= 'ת' or is_both(char)
 
 
 def is_english(char):
-    return 'a' <= char <= 'z' or 'A' <= char <= 'Z' or char == ' ' or '0' <= char <= '9' or char in string.punctuation
+    return 'a' <= char <= 'z' or 'A' <= char <= 'Z' or is_both(char)
 
 
-def move_spaces(s):
+def move_punctuations(s: str):
+    """
+    this function swipe punctuations from start to end
+    for example: '!@#abc%$#' -> '#$%abc!@#'
+    """
     if not s:
         return s
-
-    num_start_spaces = 0
-    num_end_spaces = 0
-
-    # Count leading spaces
+    start_spaces = ''
+    end_spaces = ''
     for char in s:
-        if char == " ":
-            num_start_spaces += 1
+        if char in string.punctuation + ' ':
+            start_spaces += char
         else:
             break
-
-    # Count trailing spaces
     for char in s[::-1]:
-        if char == " ":
-            num_end_spaces += 1
+        if char in string.punctuation + ' ':
+            end_spaces += char
         else:
             break
+    new_start = ''.join([REVERSE[c] if c in REVERSE else c for c in end_spaces])
+    new_end = ''.join([REVERSE[c] if c in REVERSE else c for c in start_spaces])
+    return new_start + s.lstrip(start_spaces).rstrip(end_spaces) + new_end
 
-    # Construct the modified string
-    return " " * num_end_spaces + s.strip() + " " * num_start_spaces
 
-
-def hebrew_reshaper(text):
+def hebrew_reshaper(text: str):
     sub_texts = text.split('\n')
     new_strings = []
-    for text in sub_texts:
-        last_string_puncs = ''
-        while text[-1] in string.punctuation:
-            last_string_puncs += text[-1]
-            text = text[:-1]
+    for sub_text in sub_texts:
+        if not sub_text:
+            new_strings.append('')
+            continue
 
         breaks = []
-        is_current_hebrew = is_hebrew(text[0])
+        is_current_hebrew = not is_english(sub_text[0])
+        contain_hebrew = is_current_hebrew
         current_text = ''
 
-        for char in text:
+        for char in sub_text:
             if is_current_hebrew:
                 if is_hebrew(char):
                     current_text += char
@@ -63,33 +68,31 @@ def hebrew_reshaper(text):
                     breaks.append((current_text, is_current_hebrew))
                     current_text = char
                     is_current_hebrew = True
+                    contain_hebrew = True
         breaks.append((current_text, is_current_hebrew))
         breaks = breaks[::-1]
 
-        for i in range(len(breaks)):
-            part, is_heb = breaks[i]
-            part = move_spaces(part)
-            breaks[i] = (part, is_heb)
+        if contain_hebrew:
+            for i in range(len(breaks)):
+                part, is_heb = breaks[i]
+                if part.endswith(')') and not is_heb:
+                    part = '(' + part[:-1]
+                part = move_punctuations(part)
+                breaks[i] = (part, is_heb)
         new_string = ''.join([part for part, _ in breaks])
-
-        if len(breaks) > 1:
-            new_string = last_string_puncs + new_string
-        else:
-            new_string += last_string_puncs
         new_strings.append(new_string)
     return '\n'.join(new_strings)
 
 
-def main():
-    root = tk.Tk()
-    root.title("Hebrew Reshaper")
-    text = 'זה משפט WITH ENGLISH ועברית TOGETHER!'
-    text = hebrew_reshaper(text)
-    # Create a Label widget to display the reshaped text
-    label = tk.Label(root, text=text, font=("Arial", 14))
-    label.pack(fill=tk.BOTH)
-    root.mainloop()
+# def main():
+#     """ this is some test code"""
+#     root = tk.Tk()
+#     text = "hello מה)) קורה? WITH YOU!%^ הכל טוב (נראלי)"
+#     text = hebrew_reshaper(text)
+#     label = tk.Label(root, text=text)
+#     label.pack(fill=tk.BOTH)
+#     root.mainloop()
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
